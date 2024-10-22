cee1fb1c82911b40d0acfc76aa9aff2d37fb9f64
Ekramul <mdekramulhassan168@gmail.com>
2024-10-22 20:35:02 +0600

    data added

--- Assignment7.sql
+++ Assignment7.sql
@@ -25,3 +25,31 @@ CREATE TABLE enrollment (
     student_id INTEGER REFERENCES students(student_id),
     course_id INTEGER REFERENCES courses(course_id)
 );
+
+-- student data added
+
+INSERT INTO students (student_id, student_name, age, email, frontend_mark, backend_mark, status)
+VALUES 
+(1, 'Sameer', 21, 'sameer@example.com', 48, 60, NULL),
+(2, 'Zoya', 23, 'zoya@example.com', 52, 58, NULL),
+(3, 'Nabil', 22, 'nabil@example.com', 37, 46, NULL),
+(4, 'Rafi', 24, 'rafi@example.com', 41, 40, NULL),
+(5, 'Sophia', 22, 'sophia@example.com', 50, 52, NULL),
+(6, 'Hasan', 23, 'hasan@gmail.com', 43, 39, NULL);
+
+-- courses data added 
+
+INSERT INTO courses (course_id, course_name, credits)
+VALUES 
+(1, 'Next.js', 3),
+(2, 'React.js', 4),
+(3, 'Databases', 3),
+(4, 'Prisma', 3);
+
+-- add data of enrollment
+INSERT INTO enrollment (enrollment_id, student_id, course_id)
+VALUES 
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 1),
+(4, 3, 2);
