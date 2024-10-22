d9eea9a4ffd530c57eddf7d1b8de7123bebdf436
Ekramul <mdekramulhassan168@gmail.com>
2024-10-22 20:29:25 +0600

    successfully student courses and enrollment table added

--- Assignment7.sql
+++ Assignment7.sql
@@ -1,1 +1,27 @@
 -- Active: 1729154538545@@127.0.0.1@5432@university_db
+
+-- Creating student Table 
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(100),
+    age INTEGER,
+    email VARCHAR(100),
+    frontend_mark INTEGER,
+    backend_mark INTEGER,
+    status VARCHAR(20)
+);
+
+-- CREATING course Table
+
+CREATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(100),
+    credits INTEGER
+);
+
+-- creating enrollment table 
+CREATE TABLE enrollment (
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id INTEGER REFERENCES students(student_id),
+    course_id INTEGER REFERENCES courses(course_id)
+);
